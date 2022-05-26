2d9bce3a9769a747dfe636ea130930dd9adf0c1a
gsmalves <gustavoeu2017@gmail.com>
2022-05-26 09:59:49 -0300

    adicionando campos a tabela do estacionamento

---
+++ script.ts
@@ -0,0 +1,63 @@
+interface Veiculo{
+    nome: string,
+    placa: string,
+    entrada: Date,
+}
+
+
+
+
+(function(){
+    const $ = (query: string):HTMLInputElement|null=>document.querySelector(query)
+
+
+    function patio(){
+        function ler(){
+
+        }
+    
+        function adicionar(veiculo: Veiculo){
+            const row = document.createElement("tr");
+            
+            row.innerHTML = `
+                
+                <td>${veiculo.nome}</td>
+                <td>${veiculo.placa}</td>
+                <td>${veiculo.entrada}</td>
+                <td>
+                <button class="delete" data-placa="${veiculo.placa}"></button>
+                </td>
+            `;
+
+            $("#patio")?.appendChild(row)
+            
+
+
+        }
+
+        function remover(){
+
+        }
+        function salvar(){
+
+        }
+        function render(){
+
+        }
+        return{ler, adicionar,remover, render, salvar};
+    }
+
+    $("#cadastrar")?.addEventListener("click",()=>{
+        const nome = $("#nome")?.value;
+        const placa = $("#placa")?.value;
+
+
+        if(!nome || !placa){
+            alert("Campos obrigatorios: nome e placa");
+            return;
+        }
+
+        patio().adicionar({nome,placa,entrada: new Date()})
+    })
+
+})();
